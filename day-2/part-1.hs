6df447c2dd4d9ccb2dace08f38b03fdc2a3ea226
Xuehao Xiang <Xuehao.Xiang@Outlook.com>
2023-12-28 20:10:03 -0500

    rewrite day-2

--- day-2/part-1.hs
+++ day-2/part-1.hs
@@ -1,56 +1,25 @@
-import System.IO
 import Distribution.Utils.String (trim)
-import Data.Map (Map, fromList, lookup)
-import Data.Maybe
+import Control.Arrow ((&&&))
+import MyHelpers (wordsWhen)
 
-import MyHelpers ( wordsWhen )
 
-import Prelude hiding (lookup)
-
-data Cubes = Cubes
-    {
-        red :: Int,
-        green :: Int,
-        blue :: Int
-    }
-
-maxCubes :: Cubes
-maxCubes = Cubes 12 13 14
-
-cubesLE :: Cubes -> Cubes -> Bool
-cubesLE cubes1 cubes2 = (red cubes1 <= red cubes2)
-    && (green cubes1 <= green cubes2)
-    && (blue cubes1 <= blue cubes2)
-
-cubesAdd :: Cubes -> Cubes -> Cubes
-cubesAdd (Cubes r1 g1 b1) (Cubes r2 g2 b2) = Cubes (r1 + r2) (g1 + g2) (b1 + b2)
-
-readColor :: String -> Cubes
-readColor colorStr = case color of
-    "red" -> Cubes num 0 0
-    "green" -> Cubes 0 num 0
-    "blue" -> Cubes 0 0 num
+parse :: String -> [(Int, [[(String, Int)]])]
+parse = map parseLine . lines
     where
-        subStrs = words (trim colorStr)
-        num = read (head subStrs)
-        color = last subStrs
+        parseLine = (parseGameID &&& parseReveals) . wordsWhen (==':')
+        parseGameID = read @Int . last . words . head
+        parseReveals = map parseReveal . wordsWhen (==';') . last
+        parseReveal = map parseCube . wordsWhen (==',')
+        parseCube = (last &&& (read @Int . head)) . words . trim
 
-readCubes :: String -> Cubes
-readCubes cubesStr = foldl cubesAdd (Cubes 0 0 0) $ map readColor $ wordsWhen (== ',') cubesStr
-
-readRounds :: String -> [Cubes]
-readRounds roundsStr = map readCubes $ wordsWhen (== ';') roundsStr
-
-solveCase :: String -> Int
-solveCase caseStr = if possible then gameID else 0
+solveCase :: (Int, [[(String, Int)]]) -> Int
+solveCase (gameID, reveals) = gameID * fromEnum isPossible
     where
-        subStrs = wordsWhen (== ':') caseStr
-        gameID = read $ last $ words $ head subStrs
-        possible = all (`cubesLE` maxCubes) (readRounds $ last subStrs)
+        getCubeMax color = maximum . map snd . filter ((==color) . fst) . concat
+        isPossible = all (\(color, maxVal) -> getCubeMax color reveals <= maxVal) [("red", 12), ("green", 13), ("blue", 14)]
 
+solve :: [(Int, [[(String, Int)]])] -> Int
+solve = sum . map solveCase
 
 main :: IO ()
-main = do
-    input <-  readFile "./input"
-    let result = sum $ map solveCase $ lines input
-    print result
+main = readFile "./input" >>= print . solve . parse
--- day-2/part-2.hs
+++ day-2/part-2.hs
@@ -1,58 +1,25 @@
-import System.IO
 import Distribution.Utils.String (trim)
-import Data.Map (Map, fromList, lookup)
-import Data.Maybe
+import Control.Arrow ((&&&))
+import MyHelpers (wordsWhen)
 
-import MyHelpers ( wordsWhen )
 
-import Prelude hiding (lookup)
-
-data Cubes = Cubes
-    {
-        red :: Int,
-        green :: Int,
-        blue :: Int
-    }
-
-maxCubes :: Cubes
-maxCubes = Cubes 12 13 14
-
-cubesLE :: Cubes -> Cubes -> Bool
-cubesLE cubes1 cubes2 = (red cubes1 <= red cubes2)
-    && (green cubes1 <= green cubes2)
-    && (blue cubes1 <= blue cubes2)
-
-cubesAdd :: Cubes -> Cubes -> Cubes
-cubesAdd (Cubes r1 g1 b1) (Cubes r2 g2 b2) = Cubes (r1 + r2) (g1 + g2) (b1 + b2)
-
-cubesMergeByMax :: Cubes -> Cubes -> Cubes
-cubesMergeByMax (Cubes r1 g1 b1) (Cubes r2 g2 b2) = Cubes (max r1 r2) (max g1 g2) (max b1 b2)
-
-readColor :: String -> Cubes
-readColor colorStr = case color of
-    "red" -> Cubes num 0 0
-    "green" -> Cubes 0 num 0
-    "blue" -> Cubes 0 0 num
+parse :: String -> [(Int, [[(String, Int)]])]
+parse = map parseLine . lines
     where
-        subStrs = words (trim colorStr)
-        num = read (head subStrs)
-        color = last subStrs
+        parseLine = (parseGameID &&& parseReveals) . wordsWhen (==':')
+        parseGameID = read @Int . last . words . head
+        parseReveals = map parseReveal . wordsWhen (==';') . last
+        parseReveal = map parseCube . wordsWhen (==',')
+        parseCube = (last &&& (read @Int . head)) . words . trim
 
-readCubes :: String -> Cubes
-readCubes cubesStr = foldl cubesAdd (Cubes 0 0 0) $ map readColor $ wordsWhen (== ',') cubesStr
-
-readRounds :: String -> [Cubes]
-readRounds roundsStr = map readCubes $ wordsWhen (== ';') roundsStr
-
-solveCase :: String -> Int
-solveCase caseStr = red mergedCubes * green mergedCubes * blue mergedCubes
+solveCase :: (Int, [[(String, Int)]]) -> Int
+solveCase (_, reveals) = power
     where
-        subStrs = wordsWhen (== ':') caseStr
-        mergedCubes = foldl cubesMergeByMax (Cubes 0 0 0) (readRounds $ last subStrs)
+        getCubeMax color = maximum . map snd . filter ((==color) . fst) . concat
+        power = product $ map (`getCubeMax` reveals) ["red", "green", "blue"]
 
+solve :: [(Int, [[(String, Int)]])] -> Int
+solve = sum . map solveCase
 
 main :: IO ()
-main = do
-    input <-  readFile "./input"
-    let result = sum $ map solveCase $ lines input
-    print result
+main = readFile "./input" >>= print . solve . parse
