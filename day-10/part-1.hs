4f97682b5e873d53cba8cb7ac522a56f0c57d9c0
Xuehao Xiang <Xuehao.Xiang@Outlook.com>
2024-01-04 19:47:14 -0500

    clean code

--- day-10/part-1.hs
+++ day-10/part-1.hs
@@ -1,50 +1,49 @@
 import Data.Map (Map, fromList, (!))
-import Debug.Trace (trace)
+import Control.Applicative (liftA2)
 
 
 type Coord = (Int, Int)
-type Tile = [Coord]
 
-addCoord :: Coord -> Coord -> Coord
-addCoord (x, y) (x', y') = (x + x', y + y')
-
-tileMap :: Map Char [(Int, Int)]
-tileMap = fromList [
-        ('F', [(0, 1),(1, 0)]),
-        ('-', [(0, 1),(0, -1)]),
-        ('7', [(0, -1),(1, 0)]),
-        ('|', [(1, 0),(-1, 0)]),
-        ('J', [(-1, 0),(0, -1)]),
-        ('L', [(-1, 0),(0, 1)]),
-        ('S', [(1, 0),(0, 1),(-1,0),(0,-1)]),
-        ('.', [])
-    ]
-
-parse :: String -> [[(Coord, [Coord])]]
-parse input = zipWith zip coordMat tileMat'
+parse :: String ->[[(Coord, [Coord])]]
+parse input = zipWith zip coordMat tileMat
     where
         charMat = lines input
         (nRows, nCols) = (length charMat, length $ head charMat)
         coordMat = map (\ri -> map (ri,) [1..nCols]) [1..nRows]
-        tileMat = map (map (tileMap !)) charMat
-        tileMat' = zipWith (zipWith (map . addCoord)) coordMat tileMat
+        tileMat = zipWith (zipWith parseTile) charMat coordMat
+
+        parseTile tile coord = case tile of
+            'F' -> [right coord, down coord]
+            '-' -> [right coord, left coord]
+            '7' -> [left coord, down coord]
+            '|' -> [up coord, down coord]
+            'J' -> [up coord, left coord]
+            'L' -> [up coord, right coord]
+            'S' -> [left coord, right coord, up coord, down coord] -- out of index error when S is on the edge
+            '.' -> []
+
+        left (ri, ci) = (ri, ci-1)
+        right (ri, ci) = (ri, ci+1)
+        up (ri, ci) = (ri-1, ci)
+        down (ri, ci) = (ri+1, ci)
 
 type State = (Coord, Coord)
 
-move :: Map Coord Tile -> State -> State
-move pipeMap (preCoord, currentCoord) = (currentCoord, nextCoord)
-    where 
-        pipeDests = pipeMap ! currentCoord 
-        nextCoord = head $ filter (/=preCoord) pipeDests
+walk :: Map Coord [Coord] -> State -> State
+walk pipeMap (preCoord, curCoord) = (curCoord, nextCoord)
+    where
+        nextCoord = head $ filter (/=preCoord) $ pipeMap ! curCoord
 
-solve :: [[(Coord, [Coord])]] -> Int
-solve tileMat = ceiling $ (/ 2) $ fromIntegral $ length $ takeWhile (not . (==(fst start)) . snd) $ iterate moveWithMap currentState
-    where 
+getPath :: [[(Coord, [Coord])]] -> [Coord]
+getPath tileMat = liftA2 (++) (map fst) ((:[]). snd . last) $ takeWhile ((/= startCoord) . snd) $ iterate (walk pipeMap) currentState
+    where
         pipeMap = fromList $ concat tileMat
-        moveWithMap = move pipeMap
-        start = head $ filter ((==4) . length . snd) $ concat tileMat
-        nextCoord = head $ filter (\coord -> elem (fst start) (pipeMap ! coord)) (snd start)
-        currentState = (fst start, nextCoord)
+        startCoord = fst $ head $ filter ((==4) . length . snd) $ concat tileMat
+        nextCoord = head $ filter (\coord -> startCoord `elem` (pipeMap ! coord)) (pipeMap ! startCoord)
+        currentState = (startCoord, nextCoord)
+
+solve :: [[(Coord, [Coord])]] -> Int 
+solve tileMat = ceiling $ (/ 2) $ fromIntegral $ length $ getPath tileMat
 
 main :: IO ()
 main = readFile "./input" >>= print . solve . parse
--- day-10/part-2.hs
+++ day-10/part-2.hs
@@ -1,57 +1,54 @@
 import Data.Map (Map, fromList, (!))
-import Debug.Trace (trace)
-import Control.Applicative (Applicative(liftA2))
-import qualified Data.Set as Set  
+import Control.Applicative (liftA2)
+
 
 type Coord = (Int, Int)
 
-addCoord :: Coord -> Coord -> Coord
-addCoord (x, y) (x', y') = (x + x', y + y')
-
-tileMap :: Map Char [(Int, Int)]
-tileMap = fromList [
-        ('F', [(0, 1),(1, 0)]),
-        ('-', [(0, 1),(0, -1)]),
-        ('7', [(0, -1),(1, 0)]),
-        ('|', [(1, 0),(-1, 0)]),
-        ('J', [(-1, 0),(0, -1)]),
-        ('L', [(-1, 0),(0, 1)]),
-        ('S', [(1, 0),(0, 1),(-1,0),(0,-1)]),
-        ('.', [])
-    ]
-
-parse :: String -> [[(Coord, [Coord])]]
-parse input = zipWith zip coordMat tileMat'
+parse :: String ->[[(Coord, [Coord])]]
+parse input = zipWith zip coordMat tileMat
     where
         charMat = lines input
         (nRows, nCols) = (length charMat, length $ head charMat)
         coordMat = map (\ri -> map (ri,) [1..nCols]) [1..nRows]
-        tileMat = map (map (tileMap !)) charMat
-        tileMat' = zipWith (zipWith (map . addCoord)) coordMat tileMat
+        tileMat = zipWith (zipWith parseTile) charMat coordMat
+
+        parseTile tile coord = case tile of
+            'F' -> [right coord, down coord]
+            '-' -> [right coord, left coord]
+            '7' -> [left coord, down coord]
+            '|' -> [up coord, down coord]
+            'J' -> [up coord, left coord]
+            'L' -> [up coord, right coord]
+            'S' -> [left coord, right coord, up coord, down coord] -- out of index error when S is on the edge
+            '.' -> []
+
+        left (ri, ci) = (ri, ci-1)
+        right (ri, ci) = (ri, ci+1)
+        up (ri, ci) = (ri-1, ci)
+        down (ri, ci) = (ri+1, ci)
 
 type State = (Coord, Coord)
 
-move :: Map Coord [Coord] -> State -> State
-move pipeMap (preCoord, currentCoord) = (currentCoord, nextCoord)
-    where 
-        pipeDests = pipeMap ! currentCoord 
-        nextCoord = head $ filter (/=preCoord) pipeDests
+walk :: Map Coord [Coord] -> State -> State
+walk pipeMap (preCoord, curCoord) = (curCoord, nextCoord)
+    where
+        nextCoord = head $ filter (/=preCoord) $ pipeMap ! curCoord
 
-getPipePath :: [[(Coord, [Coord])]] -> [Coord]
-getPipePath tileMat = liftA2 (++) (map fst) ((:[]) . snd . last) $ takeWhile (not . (==(fst start)) . snd) $ iterate moveWithMap currentState
-    where 
+getPath :: [[(Coord, [Coord])]] -> [Coord]
+getPath tileMat = liftA2 (++) (map fst) ((:[]). snd . last) $ takeWhile ((/= startCoord) . snd) $ iterate (walk pipeMap) currentState
+    where
         pipeMap = fromList $ concat tileMat
-        moveWithMap = move pipeMap
-        start = head $ filter ((==4) . length . snd) $ concat tileMat
-        nextCoord = head $ filter (\coord -> elem (fst start) (pipeMap ! coord)) (snd start)
-        currentState = (fst start, nextCoord)
+        startCoord = fst $ head $ filter ((==4) . length . snd) $ concat tileMat
+        nextCoord = head $ filter (\coord -> startCoord `elem` (pipeMap ! coord)) (pipeMap ! startCoord)
+        currentState = (startCoord, nextCoord)
 
+shoelace :: [(Int, Int)] -> Int
 shoelace pts = abs . sum $ zipWith (\(x1, y1) (x2, y2) -> x1 * y2 - y1 * x2) pts (drop 1 pts ++ pts)
 
-solve :: [[(Coord, [Coord])]] -> Int 
-solve pipeMat = ((shoelace pipePath) - (length pipePath)) `div` 2 + 1 
-    where 
-        pipePath = getPipePath pipeMat
+solve :: [[(Coord, [Coord])]] -> Int
+solve pipeMat = shoelace pipePath `div` 2 - length pipePath `div` 2 + 1
+    where
+        pipePath = getPath pipeMat
 
 main :: IO ()
 main = readFile "./input" >>= print . solve . parse
